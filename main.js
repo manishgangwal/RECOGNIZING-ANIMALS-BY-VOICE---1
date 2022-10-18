0eb6fcf2e8883492c6cc35753dc264afbcae94d0
NIMISHA <49507092+manishgangwal@users.noreply.github.com>
2022-10-18 18:40:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//https://teachablemachine.withgoogle.com/models/BPrucRgua/
+//<img src="animal-01.jpg" width="300" height="300">
+//<img src="animal-02.jpg" width="300" height="300">
+//<img src="animal-03.jpg" width="300" height="300">
+//<img src="animal-04.jpg" width="300" height="300">
